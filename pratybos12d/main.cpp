9473627a440532af7c5e04cf7e9df08fbb5a45d9
atsigaves <dovydas.sinkevicius1@gmail.com>
2024-09-12 15:12:51 +0300

    paruosta ketvirta

--- pratybos12d/main.cpp
+++ pratybos12d/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 {
     cout<<"-------1 uzduotis--------" << endl;
+
     string vardas = "Dovydas";
     string pavarde = "Sinkevicius";
     int amzius = 19;
@@ -12,6 +13,7 @@ int main()
     cout << "Amzius: " << amzius << endl;
 //antra uzduotis
     cout << "-------2 uzduotis-------" << endl;
+
     string krepsinioKlubas = "Vilniaus Rytas";
     int ikurimoMetai = 1997;
     string savininkas = "Darius Gudelis";
@@ -23,6 +25,21 @@ int main()
     cout << "savininkas: " << savininkas << endl;
     cout << "Arena: " << arena <<endl;
     cout << "VietuSkaicius: " << vietuSkaicius << endl;
+//trecia uzduotis
+    cout << "-------3 uzduotis-------" << endl;
+
+    string marke = "BMW";
+    string modelis = "525d";
+    int metai = 2011;
+    float litrazas = 2.5;
+    string spalva = "tamsiai sidabrines";
+
+    cout << "Automobilis: " << marke << " " << modelis;
+    cout << " yra pagamintas " << metai << ".";
+    cout << " Jo motoras " << litrazas << " litrazo.";
+    cout << " Automobilis yra " << spalva << " spalvos.";
+//ketvirta uzduotis
+    cout << "-------4 uzduotis-------" << endl;
 
 
     return 0;

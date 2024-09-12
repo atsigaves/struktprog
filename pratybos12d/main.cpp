b8233db27377160a20d1963cc0b87d157626599a
atsigaves <dovydas.sinkevicius1@gmail.com>
2024-09-12 14:54:01 +0300

    pratybos12

---
+++ pratybos12d/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    cout<<"-------1 uzduotis--------" << endl;
+    string vardas = "Dovydas";
+    string pavarde = "Sinkevicius";
+    int amzius = 19;
+    cout << "Vardas: " << vardas << endl;
+    cout << "Pavarde: " << pavarde << endl;
+    cout << "Amzius: " << amzius << endl;
+//antra uzduotis
+    cout << "-------2 uzduotis-------" << endl;
+    string krepsinioKlubas = "Vilniaus Rytas";
+    int ikurimoMetai = 1997;
+    string savininkas = "Darius Gudelis";
+    string arena = "Jeep arena";
+    int vietuSkaicius = 2500;
+
+    cout << "Krepsinio komanda: " << krepsinioKlubas << endl;
+    cout << "Ikurimo metai: " << ikurimoMetai  << endl;
+    cout << "savininkas: " << savininkas << endl;
+    cout << "Arena: " << arena <<endl;
+    cout << "VietuSkaicius: " << vietuSkaicius << endl;
+
+
+    return 0;
+}

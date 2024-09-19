b99599ca0437fbb759c5b8197f9b80eafe93b47e
atsigaves <dovydas.sinkevicius1@gmail.com>
2024-09-19 15:37:56 +0300

    k

---
+++ rugsejo19dpratybos 15.08.50/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int choice = 0;
+
+
+    while (choice !=4) {
+         cout << "paprastas meniu" << endl;
+    cout << "1. Pirma opcija" << endl;
+    cout << "2. Antra opcija" << endl;
+    cout << "3. Trecia opcija" << endl;
+    cout << "4. Baigti opcija" << endl;
+    cout << "Jusu pasirinkimas: " << endl;
+        cin >> choice;
+        switch (choice) {
+            case 1:
+                cout << "Jus pasirinkote pirma opcija" << endl;
+            break;
+            case 2:
+                cout << "Jus pasirinkote antra opcija" << endl;
+            break;
+            case 3:
+                cout << "Jus pasirinkote trecia opcija" << endl;
+            break;
+            case 4:
+                cout << "Jus baigete programa" << endl;
+            default:
+                cout << "Tokios opcijos nera. Bandykite dar karta." << endl;
+            ;
+        }
+    }
+    return 0;
+}
---
+++ rugsejo19duzduotis/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int pazym = 4;
+    int pazym2 = 7;
+    int pazym3 = 10;
+    int pazym4 = 5;
+
+
+    double sum = pazym + pazym2 + pazym3 + pazym4;
+    double vidurkis = sum / 4;
+    if (vidurkis >=5) {
+        cout << "Vidurkis teigiamas " << vidurkis << endl;
+
+    } else {
+        cout << "Vidurkis neigiamas " << vidurkis << endl;
+    }
+
+
+
+
+
+
+    return 0;
+}

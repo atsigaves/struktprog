172424226d9ac078d2a72777555796f417405969
atsigaves <dovydas.sinkevicius1@gmail.com>
2024-11-28 00:59:51 +0200

    Abeceles desifravimas

--- 3PraktinisDarbas/main.cpp
+++ 3PraktinisDarbas/main.cpp
@@ -31,8 +31,45 @@ string sifravimasABC(char tekstas[], char raktas[]) {
             }
         }
     }
+    string rezultatasString = "";
+for (int i = 0; i < tekstoIlgis; i++) {
+    rezultatasString = rezultatasString + rezultatas[i];
 }
-
+return rezultatasString;
+}
+string desifravimasABC(char tekstas[], char raktas[]) {
+    int tekstoIndeksas = -1;
+    int raktoIndeksas = -1;
+    int rezultatoIndeksas;
+    int tekstoIlgis = strlen(tekstas);
+    string raktasString = raktas;
+    while(raktasString.length() < strlen(tekstas)) {
+        raktasString.append(raktasString.begin(), raktasString.end());
+    }
+    strcpy(raktas, raktasString.c_str());
+    char rezultatas[100];
+    for (int i = 0; i < tekstoIlgis; i++) {
+        for (int j = 0; j < sizeof(abecele); j++) {
+            if (toupper(tekstas[i]) == abecele[j]) {
+                tekstoIndeksas = j;
+            }
+            if (toupper(raktas[i]) == abecele[j]) {
+                raktoIndeksas = j;
+            }
+            if (tekstoIndeksas != -1 && raktoIndeksas != -1) {
+                rezultatoIndeksas = (tekstoIndeksas - raktoIndeksas + sizeof(abecele)) % sizeof(abecele);
+                rezultatas[i] = abecele[rezultatoIndeksas];
+                tekstoIndeksas = -1;
+                raktoIndeksas = -1;
+            }
+        }
+    }
+    string rezultatasString = "";
+    for (int i = 0; i < tekstoIlgis; i++) {
+        rezultatasString = rezultatasString + rezultatas[i];
+    }
+    return rezultatasString;
+}
 
 
 

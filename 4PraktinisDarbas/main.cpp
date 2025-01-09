b42bf97738324dab1103fdb4d3a790b2ba8dd46b
atsigaves <dovydas.sinkevicius1@gmail.com>
2025-01-09 21:45:12 +0200

    menu kurimas

--- 4PraktinisDarbas/main.cpp
+++ 4PraktinisDarbas/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <regex>
 #include <cmath>
 using namespace std;
 
@@ -15,18 +16,106 @@ void Kiekiai(int kiekiai[]) {
     }
 }
 
+void tekstoAtskyrimas(string& tekstas, string line, double& skaicius) {
+    regex numberRegex(R"((.*?)(-?\d+(\.\d+)?))");
+    smatch sutapimas;
+    if (regex_search(line, sutapimas, numberRegex)) {
+        tekstas = sutapimas[1].str();
+        skaicius = stod(sutapimas[2].str());
+    }
+}
 
+int ilgasString(menuItemType menu[]) {
+    int ilgas = 0;
+    for (int i = 0; i < sizeof(menu); i++) {
+        if (menu[i].menuItem.length() > ilgas) {
+            ilgas = menu[i].menuItem.length();
+        }
+    }
+    return ilgas;
+}
 
+void getData(menuItemType menu[]) {
+    string line;
+    ifstream maistas("maistas.txt");
+    int x = 0;
+    while (getline(maistas, line)) {
+        tekstoAtskyrimas(line, menu[x].menuItem, menu[x].menuPrice);
+        x++;
+    }
+    maistas.close();
+}
 
+void showMenu (menuItemType menu[]) {
+    cout << "Sveiki atvyke i restorana 'pietutis' " << endl;
+    cout << "Pasirinkite maista paspaudus jo skaiciu" << endl;
+    for (int i = 0; i < sizeof(menu); i++) {
+        int tarpai = ilgasString(menu)-menu[i].menuItem.length();
+        menu[i].menuItem.append(tarpai,'');
+        cout << fixed << setprecision(2) << i+1 << ". " << menu[i].menuItem << " " << menu[i].menuPrice << "e" << endl;
+    }
+    cout << "9. " << "cekis" << endl;
+    cout << "Jusu pasirinkimas" << endl;
+}
 
-
-
-
-
-
-
+void spausdintiCeki(menuItemType menu[], int kiekiai[]) {
+    ofstream fout("cekis.txt");
+    double sum = 0, mokesciuSk = 0, galutine = 0;
+    fout << "Sveiki atvyke i 'pietutis'" << endl;
+    for (int i = 0; i < sizeof(menu); i++) {
+        if (kiekiai[i] > 0 ) {
+            int tarpai = ilgasString(menu)-menu[i].menuItem.length();
+            fout << fixed << setprecision(2) << kiekiai[i] << " " << menu[i].menuItem << menu[i].menuPrice * kiekiai[i] << "e" << endl;
+            sum += menu[i].menuPrice * kiekiai[i];
+        }
+    }
+    mokesciuSk = sum * 0.21;
+    galutine = mokesciuSk + sum;
+    string mokesciai = "mokesciai";
+    string galutine_suma = "galutine_suma";
+    int tarpai = ilgasString(menu)-mokesciai.length()+2;
+    galutine_suma.append(tarpai,'');
+    fout << fixed << setprecision(2) << mokesciai << mokesciuSk << "e" << endl;
+    fout << fixed << setprecision(2) << galutine_suma << galutine << "e" << endl;
+    fout.close();
+}
 
 int main() {
-
-    return 0;
-}
+    menuItemType menuList[8];
+    int kiekiai[8];
+    int pasirinkimas;
+    Kiekiai(kiekiai);
+    getData(menuList);
+    while (pasirinkimas != 9) {
+        showMenu(menuList);
+        cin >> pasirinkimas;
+        switch (pasirinkimas) {
+            case 1:
+                kiekiai[0]++;
+            break;
+            case 2:
+                kiekiai[1]++;
+            break;
+            case 3:
+                kiekiai[2]++;
+            break;
+            case 4:
+                kiekiai[3]++;
+            break;
+            case 5:
+                kiekiai[4]++;
+            break;
+            case 6:
+                kiekiai[5]++;
+            break;
+            case 7:
+                kiekiai[6]++;
+            break;
+            case 8:
+                kiekiai[7]++;
+            break;
+        }
+    }
+    spausdintiCeki(menuList, kiekiai);
+        return 0;
+    }

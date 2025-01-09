d70a079e114723fa807b9605ef232c89e9e0d3eb
atsigaves <dovydas.sinkevicius1@gmail.com>
2025-01-09 18:01:26 +0200

    kiekiu nulinimas

---
+++ 4PraktinisDarbas/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cmath>
+using namespace std;
+
+struct menuItemType {
+    string menuItem;
+    double menuPrice;
+};
+
+void Kiekiai(int kiekiai[]) {
+    for (int i = 0; i < sizeof(kiekiai); i++) {
+        kiekiai[i] = 0;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+int main() {
+
+    return 0;
+}
--- Lapkricio8-15dPratybos/main.cpp
+++ Lapkricio8-15dPratybos/main.cpp
@@ -7,19 +7,40 @@ int main()
     int masyvas [4];
     int k = 0;
     int suma;
-    ifstream failas;
+    int masyvas1 [11];
+    int l = 0;
+    int suma1;
+    int x, y;
+    masyvas1[10] = x;
+    y = 2;
+    /*ifstream failas;
     failas.open("input.txt");
     while (!failas.eof()) {
         failas >> masyvas[k];
         k++;
     }
-    cout << endl;
     for (int i = 0; i < k; i++) {
         cout << masyvas[i] << endl;
 
     }
     suma = masyvas[0] + masyvas[1] + masyvas[2] + masyvas[3] + masyvas[4];
-    cout << endl;
-    cout << "Susumuoti skaiciai is failo: " << suma << endl;
+   ofstream resul;
+    resul.open("output.txt");
+    resul << suma << endl;
+    resul.close();
+    */
+    // antra uzduotis
+    ifstream failas1;
+    failas1.open("input1.txt");
+    while (failas1.eof()) {
+        failas1 >> masyvas1[l];
+        l++;
+    }
+    for (int i = 0; i < x; i++) {
+        cout << x << endl;
+
+    }
+
+
     return 0;
 }

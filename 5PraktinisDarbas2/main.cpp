0ea8ed9b8c68365779fa9996f8d4ecceb2b9c03e
atsigaves <dovydas.sinkevicius1@gmail.com>
2025-01-10 09:34:57 +0200

    pirmas case

--- 5PraktinisDarbas2/main.cpp
+++ 5PraktinisDarbas2/main.cpp
@@ -11,6 +11,24 @@ int main()
 {
     klientas *masyvas;
     int x;
-    cout << "pasirinkite"
+    cout << "pasirinkite klientu kieki: " << endl;
+    cin >> x;
+    masyvas = new klientas[x];
+    int kiekis = -1;
+    int pasirinkimas;
+    int trintiId, trinimoIndex;
+    while (pasirinkimas != 4) {
+        cout << "____Pasirinkite varianta is menu____" << endl;
+        cout << "1. Prideti kliento duomenis" << endl;
+        cout << "2. Atspausdinti visus kontaktus" << endl;
+        cout << "3. Istrinti studenta pagal ID" << endl;
+        cout << "4. Iseiti" << endl;
+        cout << "Pasirinkimas - "  << endl;
+        cin >> pasirinkimas;
+        switch (pasirinkimas) {
+            case 1:
+                cout << "Iveskite studento ID" << endl;
+            cin >> masyvas
+    }
     return 0;
 }
